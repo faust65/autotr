c3d62738313d9ff8b437f74331f964553eb2295e
faust65 <151982135+faust65@users.noreply.github.com>
2025-11-17 15:51:04 +0900

    Add files via upload

---
+++ autotr2.py
@@ -0,0 +1,35 @@
+import pyperclip
+import keyboard
+import time
+
+'''
+프로그램을 실행하고 복사하는 모든 항목을 붙여넣을 때 desc를 붙여줍니다
+문장에 판정이 들어가면 emas를 붙여줍니다
+불친절합니다
+개선점 있으면 주십시오 저도쓰게
+'''
+
+pt=""
+
+while 1:
+    
+    try:
+        if keyboard.is_pressed('esc'):
+            print("종료")
+            break
+        
+        ct=pyperclip.paste()
+    
+        if pt!=ct:
+            pt=ct
+        
+            if "판정" in pt :
+                pt='/emas " " '+pt
+            else :
+                pt='/desc '+pt
+        
+            pyperclip.copy(pt)
+            time.sleep(0.5)
+    except :
+        print("종료됨")
+        break
