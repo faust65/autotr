3bddcdda3fd253b6f6aec86ac82a28294d05f510
faust65 <151982135+faust65@users.noreply.github.com>
2025-11-17 16:34:04 +0900

    ㅁㅁ

--- autotr2.py
+++ autotr2.py
@@ -1,6 +1,9 @@
 import pyperclip
 import keyboard
 import time
+from flask import Flask, request, jsonify, render_template
+
+app=Flask(__name__)
 
 '''
 프로그램을 실행하고 복사하는 모든 항목을 붙여넣을 때 desc를 붙여줍니다
@@ -9,27 +12,34 @@ import time
 개선점 있으면 주십시오 저도쓰게
 '''
 
-pt=""
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-while 1:
+
+@app.route('/process', methods=['POST'])
+    def process():
+        pt=""
+
+        while 1:
     
-    try:
-        if keyboard.is_pressed('esc'):
-            print("종료")
+            try:
+                if keyboard.is_pressed('esc'):
+                    print("종료")
+                break
+        
+            ct=pyperclip.paste()
+    
+            if pt!=ct:
+                pt=ct
+        
+                if "판정" in pt :
+                    pt='/emas " " '+pt
+                else :
+                    pt='/desc '+pt
+        
+                pyperclip.copy(pt)
+                time.sleep(0.5)
+        except :
+            print("종료됨")
             break
-        
-        ct=pyperclip.paste()
-    
-        if pt!=ct:
-            pt=ct
-        
-            if "판정" in pt :
-                pt='/emas " " '+pt
-            else :
-                pt='/desc '+pt
-        
-            pyperclip.copy(pt)
-            time.sleep(0.5)
-    except :
-        print("종료됨")
-        break
