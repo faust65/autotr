daa7314be813374925ac91f894058f063e394f3d
faust65 <151982135+faust65@users.noreply.github.com>
2025-11-18 15:23:23 +0900

    cmmsg

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import pyperclip
 import keyboard
 import time
+import threading
 from flask import Flask, request, jsonify, render_template
 
 app=Flask(__name__)
@@ -37,13 +38,18 @@ def loop():
 def index():
     return render_template('index.html')
 
-
 @app.route('/process', methods=['POST'])
 def process():
-    
+    global running
+
+    if not running:
+        running=True
+        threading.thread(target=loop, daemon=True).start()
 
 @app.route('/stop', methods=['POST'])
 def stop():
+    global running
+    running=False
 
 if __name__ == "__main__":
     app.run()
